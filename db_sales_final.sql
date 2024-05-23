4784f501ededde1c198acf15c458b599d1e05650
rommel dalisay <rommelmdalisay@gmail.com>
2024-05-23 09:37:39 +0800

    new db

--- db_sales_final.sql
+++ db_sales_final.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE db_sales_final2024-s;
-USE db_sales_final2024;
+CREATE DATABASE db_sales_final2024_s;
+USE db_sales_final2024_s;
 
 CREATE TABLE customer (
     customer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -16,7 +16,7 @@ CREATE TABLE item (
     item_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     description varchar(64) NOT NULL,
     cost_price decimal(7,2),
-    cost_price decimal(5,2)
+    sell_price decimal(5,2)
 );
 
 CREATE TABLE orderinfo (
@@ -42,8 +42,15 @@ CREATE TABLE stock (
     item_id INT NOT NULL PRIMARY KEY,
     quantity TINYINT,
     CONSTRAINT stock_item_id_fk FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
 
-)
+CREATE TABLE barcode (
+    barcode_ean char(13) NOT NULL PRIMARY KEY,
+    item_id INT NOT NULL,
+    INDEX(item_id),
+    CONSTRAINT barcode_item_id_fk FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
+
 
 
 
